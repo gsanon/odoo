313d37fea95c82812db00f5b994bc5d13b80ad64
Guillaume Sanon <guillaume@goldaia.com>
2025-08-16 21:10:59 +0200

    Add applicant image urls

--- addons/custom_contact_fields/models/res_partner.py
+++ addons/custom_contact_fields/models/res_partner.py
@@ -98,3 +98,7 @@ class ResPartner(models.Model):
     x_objectives = fields.Text(_('Objectives'))
     x_issues = fields.Text(_('Issues'))
     x_need_help = fields.Boolean(_('Need Help'), default=False)
+    
+    # Image URL fields for applicant uploads
+    x_applicant_image_1_url = fields.Char(_('Applicant Image 1 URL'), help=_('URL link to first uploaded image from applicant'))
+    x_applicant_image_2_url = fields.Char(_('Applicant Image 2 URL'), help=_('URL link to second uploaded image from applicant'))
